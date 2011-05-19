1bb3721291f34c020b4cabcf97ed8fa001c0c33c
Chris Koenig <chris.koenig@microsoft.com>
2011-05-19 13:19:32 -0500

    working on details page

---
+++ BoyScouts/MeritBadgeDetailsPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace BoyScouts
+{
+    public partial class MeritBadgeDetailsPage : PhoneApplicationPage
+    {
+        public MeritBadgeDetailsPage()
+        {
+            InitializeComponent();
+        }
+
+        private void PhoneApplicationPage_OrientationChanged(object sender, OrientationChangedEventArgs e)
+        {
+            if (Orientation == PageOrientation.Landscape ||
+                Orientation == PageOrientation.LandscapeLeft ||
+                Orientation == PageOrientation.LandscapeRight)
+            {
+                //TODO: Replace with a storyboard
+                LandscapeLayout.Visibility = Visibility.Visible;
+                PortraitLayout.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                //TODO: Replace with a storyboard
+                PortraitLayout.Visibility = Visibility.Visible;
+                LandscapeLayout.Visibility = Visibility.Collapsed;
+            }
+        }
+    }
+}
---
+++ BoyScouts/ViewModels/MeritBadgeDetailsViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System.Windows.Media;
+using BoyScouts.Messages;
+using BoyScouts.Models;
+using GalaSoft.MvvmLight;
+
+namespace BoyScouts.ViewModels
+{
+    public class MeritBadgeDetailsViewModel : ViewModelBase
+    {
+        public MeritBadgeDetailsViewModel()
+        {
+            MessengerInstance.Register<MeritBadgeSelectedMessage>(this, (message) => this.MeritBadgeObject = message.MeritBadge);
+            if (IsInDesignMode)
+            {
+                // Code runs in Blend --> create design time data.
+            }
+            else
+            {
+                // Code runs "for real": Connect to service, etc...
+            }
+        }
+
+        public SolidColorBrush MeritBadgeBorderColor
+        {
+            get
+            {
+                if (MeritBadgeObject == null)
+                    return null;
+                return MeritBadgeObject.IsEagleRequired
+                    ? (SolidColorBrush)App.Current.Resources["BoyScoutRed"]
+                    : (SolidColorBrush)App.Current.Resources["BoyScoutBlue"];
+            }
+        }
+
+        public override void Cleanup()
+        {
+            // Clean own resources if needed
+            this.MeritBadgeObject = null;
+            base.Cleanup();
+        }
+
+        #region MeritBadgeObject
+
+        private MeritBadge _meritBadgeObject = null;
+
+        public MeritBadge MeritBadgeObject
+        {
+            get
+            {
+                return _meritBadgeObject;
+            }
+
+            set
+            {
+                if (_meritBadgeObject == value)
+                {
+                    return;
+                }
+
+                var oldValue = _meritBadgeObject;
+                _meritBadgeObject = value;
+                RaisePropertyChanged(() => this.MeritBadgeObject);
+            }
+        }
+
+        #endregion MeritBadgeObject
+    }
+}
