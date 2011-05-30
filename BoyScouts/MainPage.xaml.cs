4c1d5eff923cdf00131ec735a8d5c9c455e4a617
Chris Koenig <chris.koenig@microsoft.com>
2011-05-30 12:20:27 -0500

    wip

--- BoyScouts/MainPage.xaml.cs
+++ BoyScouts/MainPage.xaml.cs
@@ -15,6 +15,20 @@ namespace BoyScouts
             InitializeComponent();
         }
 
+        protected override void OnBackKeyPress(System.ComponentModel.CancelEventArgs e)
+        {
+            if (LayoutRoot.SelectedIndex != 0)
+            {
+                var targetItem = LayoutRoot.Items[0] as PanoramaItem;
+                LayoutRoot.DefaultItem = targetItem;
+                e.Cancel = true;
+            }
+            else
+            {
+                base.OnBackKeyPress(e);
+            }
+        }
+
         private void ContentButton_Click(object sender, RoutedEventArgs e)
         {
             Button button = sender as Button;
