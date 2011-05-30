d9b5a9cd2ed7ad183b672bb8cbf5c52f3b72ce6f
Chris Koenig <chris.koenig@microsoft.com>
2011-05-30 17:05:57 -0500

    wip

---
+++ BoyScouts/Models/Group.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BoyScouts.Models
+{
+    public class Group<T> : IEnumerable<T>
+    {
+        public Group(string name, IEnumerable<T> items)
+        {
+            this.Title = name;
+            this.Items = new List<T>(items);
+        }
+
+        public override bool Equals(object obj)
+        {
+            Group<T> that = obj as Group<T>;
+
+            return (that != null) && (this.Title.Equals(that.Title));
+        }
+
+        public string Title
+        {
+            get;
+            set;
+        }
+
+        public IList<T> Items
+        {
+            get;
+            set;
+        }
+
+        #region IEnumerable<T> Members
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return this.Items.GetEnumerator();
+        }
+
+        #endregion IEnumerable<T> Members
+
+        #region IEnumerable Members
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return this.Items.GetEnumerator();
+        }
+
+        #endregion IEnumerable Members
+    }
+}
